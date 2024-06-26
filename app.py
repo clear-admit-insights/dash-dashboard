e2838b24dab8c91420f74ec0c0c01f44903c967e
clear-admit-insights <insights@clearadmit.com>
2024-06-26 10:57:04 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+
+app = Dash()
+
+app.layout = [
+    html.H1(children='Population Trends by Country', style={'textAlign':'center'}),
+    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Graph(id='graph-content')
+]
+
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_graph(value):
+    dff = df[df.country==value]
+    fig = px.line(dff, x='year', y='pop')
+    fig.update_layout(
+        xaxis_title='Year',
+        yaxis_title='Total Population'
+    )
+    return fig
+
+if __name__ == '__main__':
+    app.run(debug=True)
